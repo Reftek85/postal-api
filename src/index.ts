0a0f41b99c75af2c7c18af262bca6a98e6214c55
Reftek85 <stephane.cote.qc@gmail.com>
2024-04-19 19:25:44 -0400

    old prod api

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,8 @@ import { Elysia, t } from "elysia";
 import { db } from "./db";
 import { randomUUID } from "crypto";
 import { addressEndpoints, credentials, routes } from "./schema";
-import { pgTable, serial, text, integer, boolean } from 'drizzle-orm/pg-core';
-import { eq, sql, relations } from "drizzle-orm";
+import { eq, sql } from "drizzle-orm";
 import { randomAlphaNumeric } from "./utils";
-import { randomAlphaNumSymbol } from "./utils";
-import { routesRelations, credentialsRelations  } from "./utils";
-import { generatekey, fetchRouteAndAddressEndpoint } from "./utils";
 import { cors } from "@elysiajs/cors";
 import swagger from "@elysiajs/swagger";
 import "./env";
@@ -21,7 +17,7 @@ const app = new Elysia()
     async ({ body }) => {
       const { serverId, name } = body;
       const uuid = randomUUID();
-      const key = generatekey(16);
+      const key = randomAlphaNumeric(24);
       const [{ insertId }] = await db.insert(credentials).values({
         createdAt: sql`CURRENT_TIMESTAMP`,
         updatedAt: sql`CURRENT_TIMESTAMP`,
@@ -33,7 +29,6 @@ const app = new Elysia()
         key,
       });
       return {
-        name,
         key,
         credentialId: insertId,
       };
@@ -62,10 +57,7 @@ const app = new Elysia()
       };
     },
     {
-      body: t.Object({ 
-        address: t.String(), 
-        serverId: t.Number() 
-      }),
+      body: t.Object({ address: t.String(), serverId: t.Number() }),
     }
   )
   .post(
@@ -131,176 +123,11 @@ const app = new Elysia()
     }
     return data || {};
   })
-  .post(
-    "/create",
-    async ({ body }) => {
-      const { serverId, name, address, domainId } = body;
-
-      // Check if a route with the same name already exists
-      const existingRoute = await db.query.routes.findFirst({
-        where: eq(routes.name, name),
-      });
-      if (existingRoute) {
-        // If a route with the same name exists, return an error response
-        return {
-          error: `Route with the same name ${name} already exists. Use /update instead`,
-        };
-      }
-      
-      // Check if a credential with the same name already exists
-      const existingCredential = await db.query.credentials.findFirst({
-        where: eq(credentials.name, name),
-      });
-      if (existingCredential) {
-        // If a credential with the same name exists, return an error response
-        return {
-          error: `Credential with the same name ${name} already exists. Use /update instead`,
-        };
-      }
-
-      // Create credential
-      const credentialUUID = randomUUID();
-      const credentialKey = generatekey(16);
-      const [{ insertId: credentialId }] = await db.insert(credentials).values({
-        createdAt: sql`CURRENT_TIMESTAMP`,
-        updatedAt: sql`CURRENT_TIMESTAMP`,
-        type: "SMTP",
-        serverId,
-        name,
-        uuid: credentialUUID,
-        hold: 0,
-        key: credentialKey,
-      });
-
-      // Create address
-      const addressUUID = randomUUID();
-      const [{ insertId: addressId }] = await db.insert(addressEndpoints).values({
-        createdAt: sql`CURRENT_TIMESTAMP`,
-        updatedAt: sql`CURRENT_TIMESTAMP`,
-        address,
-        uuid: addressUUID,
-        serverId,
-      });
-
-      // Create route
-      const routeUUID = randomUUID();
-      const routeToken = randomAlphaNumeric(8);
-      const [{ insertId: routeId }] = await db.insert(routes).values({
-        createdAt: sql`CURRENT_TIMESTAMP`,
-        updatedAt: sql`CURRENT_TIMESTAMP`,
-        spamMode: "Mark",
-        name,
-        serverId,
-        domainId,
-        endpointId: addressId,
-        endpointType: "AddressEndpoint",
-        token: routeToken,
-        uuid: routeUUID,
-        mode: "Endpoint",
-      });
-
-      return {
-        credential: {
-          name,
-          key: credentialKey,
-          credentialId,
-        },
-        address: {
-          addressId,
-        },
-        route: {
-          routeId,
-          routeEmail: `${routeToken}@${process.env.SERVER_ROUTES_DOMAIN}`,
-        },
-      };
-    },
-    {
-      body: t.Object({
-        serverId: t.Number(),
-        name: t.String(),
-        address: t.String(),
-        domainId: t.Number(),
-      }),
-    }
-  )
-  .post(
-    "/updatecredential",
-    async ({ body }) => {
-      const { name , serverId } = body;
-  
-      // Find the first credential with the given name
-      const existingCredential = await db.query.credentials.findFirst({
-        where: eq(credentials.name, name)
-      });
-  
-      if (existingCredential) {
-        // Generate a new key for the credential
-        const credentialKey = generatekey(16);
-  
-        // Update the credential record with the new key and other fields
-        await db.update(credentials)
-          .set({
-            key: credentialKey,
-            updatedAt: sql`CURRENT_TIMESTAMP`,
-            hold: 0,
-          })
-          .where({ id: existingCredential.id as unknown }); // Specify the id of the existingCredential here
-          
-        // Return the updated credential information
-        return {
-          credential: {
-            name,
-            key: credentialKey,
-            credentialId: existingCredential.id, // Return the credential's id
-            serverId: existingCredential.serverId
-          },
-        };
-      } else {
-        // If the credential doesn't exist, return an error response
-        return {
-          error: `Credential with name ${name} not found.`,
-        };
-      }
-    },
-    {
-      body: t.Object({
-        serverId: t.Number(),
-        name: t.String(),      
-      }),
-    }
-  )
-  .post(
-    "/updateaddress",
-    async ({ body }) => {
-        const { name, address, serverid } = body;
-
-        // Call fetchRouteAndAddressEndpoint to update the address
-        const updatedDetails = await fetchRouteAndAddressEndpoint(name, address, serverid);
-
-        if (!updatedDetails) {
-            return { error: `Failed to update address for route with name ${name}.` };
-        }
-
-        // Return the updated endpoint information
-        return {
-            addressEndpoint: {
-                name: updatedDetails.name,
-                Newaddress: address,
-                serverid: serverid,
-            }
-        };
-    },
-    {
-        body: t.Object({
-            serverid: t.String(),
-            name: t.String(),
-            address: t.String()          
-        }),
-    }
-)
-
   .listen(3000);
-
+//listen({
+//   hostname: "0.0.0.0",
+//    port: 3000,
+//  })
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
 );
